2566374bc2cefce71155ecad6109899bd98cbd82
Jesús Cintas <jcintas@viewnext.com>
2019-11-21 22:17:57 +0100

    Subida del fichero jenkinsfile

---
+++ src/test/java/com/cursoci/www/UsuarioWizardTest.java
@@ -0,0 +1,51 @@
+package com.cursoci.www;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.cursoci.www.entidades.Usuario;
+
+import junit.framework.TestCase;
+
+public class UsuarioWizardTest {
+	
+	UsuarioWizard userWizard;
+	
+	@BeforeClass
+	public void iniciar() {
+		userWizard = new UsuarioWizard();
+	}
+
+	@Test
+	public void testGetUser() {
+		final Usuario user = userWizard.getUser();
+		
+		assertNotNull(user);
+	}
+
+	@Test
+	public void testSetUser() {
+		final Usuario user = new Usuario();
+		userWizard.setUser(user);
+		
+		assertEquals(user, userWizard.getUser());
+	}
+
+	@Test
+	public void testIsSkip() {
+		assertEquals(userWizard.isSkip(), false);
+	}
+
+	@Test
+	public void testSetSkip() {
+		userWizard.setSkip(true);
+		
+		assertTrue(userWizard.isSkip());
+	}
+}
