1f9a1f22457da21e439d6535dd79769608b42e83
jcintas <jesuscintasleal@gmail.com>
2019-11-26 20:19:46 +0100

    Update configDes.groovy

--- jobs/configDes.groovy
+++ jobs/configDes.groovy
@@ -1,9 +1,8 @@
 job('Checkout Repositorio Des') {
     scm {
         git{
-                remote {
-                github('jcintas/ejercicioSemilla', 'ssh')
-                credentials('38233920-c9bf-4604-a74d-de8f701868e3')
+            remote {
+                url('https://github.com/jcintas/ejercicioSemilla.git')
             }
              branch('desarrollo')
         }
