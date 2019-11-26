389563c92e42bb2aa2a1076c2858d68f674fde94
jcintas <jesuscintasleal@gmail.com>
2019-11-26 01:34:24 +0100

    Create configuracion.groovy

---
+++ jobs/configuracion.groovy
@@ -0,0 +1,9 @@
+pipelineJob('example') {
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/jcintas/ejercicioSemilla.git', branch: "desarrollo")
+            }
+        }
+    }
+}
